7784c07cb7d5e47b778ad92a17ac85b70b043dec
Weiyi Shang <wshang@blackberry.com>
2020-01-27 15:27:01 -0500

    handle the empty input case

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
 
 public class Calculator {
-
+	ArrayList<Double> numbers=new ArrayList<Double>();
+	
 	public void add(double d) {
-		// TODO Auto-generated method stub
-		
+		numbers.add(d);
 	}
 
-	public Double average() {
-		// TODO Auto-generated method stub
-		return null;
+	public double average() throws EmptyException {
+		if(numbers.isEmpty())
+		{
+			throw new EmptyException();
+		}
+			
+		Double sum=0.0;
+		for(Double number:numbers)
+		{
+			sum+=number;
+		}
+		
+		double result=sum/numbers.size();
+		
+		System.err.println(result);
+		return result;
 	}
 
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -13,9 +13,25 @@ public class CalculatorTest {
 		c.add(30.0);
 		c.add(4.0);
 		
-		Double result=c.average();
-		assertEquals((Double)26.0, result);
+		Double result;
+		try {
+			result = c.average();
+			assertEquals((Double)26.0, result);
+		} catch (EmptyException e) {
+		}
+	}
+	
+	@Test
+	public void testEmpty()
+	{
+		Calculator c=new Calculator();
+		try {
+			c.average();
+			fail("this should fail");
 
+		} catch (EmptyException e) {
+		
+		}
 		
 	}
 
