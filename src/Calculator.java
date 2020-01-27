ae858e86d49119ef4557169bde573ba91b96ae70
Weiyi Shang <wshang@blackberry.com>
2020-01-27 15:55:23 -0500

    add median calculation and test

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Calculator {
 	ArrayList<Double> numbers=new ArrayList<Double>();
@@ -23,6 +24,38 @@ public class Calculator {
 		
 		System.err.println(result);
 		return result;
+	}
+
+	
+
+	public Double median() {
+		int sizeOfInput=numbers.size();
+		Collections.sort(numbers);
+//		for(int i=1; i< sizeOfInput; i++)
+//		{
+//			for(int j=2;j<sizeOfInput;j++)
+//			{
+//				if(numbers.get(i)>numbers.get(j))
+//				{
+//					Double temp=numbers.get(i);
+//					numbers.set(i, numbers.get(j));
+//					numbers.set(j, temp);
+//				}
+//			}
+//		}
+		
+		int medianIndex=sizeOfInput/2;
+		
+		System.err.println(medianIndex);
+		if(numbers.size() %2==	1)
+		{	
+			return numbers.get(medianIndex);
+		}
+		else
+		{
+			return (numbers.get(medianIndex)+numbers.get(medianIndex-1))/2;
+		}
+		// TODO Auto-generated method stub
 	}
 
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -5,6 +5,36 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
+	public void testMeidian()
+	{
+		Calculator c=new Calculator();
+		c.add(1.0);
+		c.add(5.0);
+		c.add(90.0);
+		c.add(30.0);
+		c.add(4.0);
+		Double result=c.median();
+		
+		System.err.println(result);
+		assertEquals((Double)5.0, result);
+	}
+	@Test
+	public void testMeidianWithEvennumber()
+	{
+		Calculator c=new Calculator();
+		c.add(1.0);
+		c.add(5.0);
+		c.add(30.0);
+		c.add(4.0);
+		Double result=c.median();
+		
+		//System.err.println(result);
+		assertEquals((Double)4.5, result);
+	}
+	
+	
+	
+	@Test
 	public void testAverage() {
 		Calculator c=new Calculator();
 		c.add(1.0);
